be8cec03e658b973ae8a39782a3360f461318b84
Nicole O'Brien <nobrien@uwaterloo.ca>
2018-05-19 22:27:15 -0400

    Block bootstrap function updated

--- R/bbs.R
+++ R/bbs.R
@@ -6,7 +6,7 @@
 #'
 #' @importFrom boot tsboot boot.ci
 #'
-#' @usage bbs(x,ci=0.95,method=c("mk","sr"),nsim=2000,eta=1)
+#' @usage bbs(x, ci=0.95, method=c("mk"), nsim=2000, eta=1, bl.len=NULL)
 #'
 #' @param  x  - Time series data vector
 #'
@@ -47,7 +47,7 @@
 #'
 #' @export
 #'
-bbs <- function(x,ci=0.95,method=c("sr"),nsim=40,eta=1) {
+bbs <- function(x,ci=0.95,method=c("mk"),nsim=200,eta=1, bl.len=NULL) {
   # Initialize the test Parameters
 
   # Time-Series Vector
@@ -74,42 +74,51 @@ bbs <- function(x,ci=0.95,method=c("sr"),nsim=40,eta=1) {
     stop("Input vector must contain at least four values")
   }
 
-  # To test whether the data values are finite numbers and attempting to eliminate non-finite numbers
+  #Specify minimum block length
+  if (is.null(bl.len) == FALSE)
+    if (bl.len > n) {
+      stop("Block length must be less than the time series length")
+    }
 
+  # To test whether the data values are finite numbers and attempting to eliminate non-finite numbers
   if (any(is.finite(x) == FALSE)) {
     x[-c(which(is.finite(x) == FALSE))] -> x
     warning("The input vector contains non-finite numbers. An attempt was made to remove them")
   }
 
-  #bounds of the confidence intervals of the acf function
+  if (is.null(bl.len) == TRUE) {
+    #bounds of the confidence intervals of the acf function
 
-  bd <- qnorm((1 + ci)/2)/sqrt(n)
+    bd <- qnorm((1 + ci)/2)/sqrt(n)
 
-  # Calculating auto-correlation function of the ranks of observations (ro)
+    # Calculating auto-correlation function of the ranks of observations (ro)
 
-  ro <- acf(x, lag.max=round(n/4), plot=T)$acf[-1]
+    ro <- acf(x, lag.max=round(n/4), plot=FALSE)$acf[-1]
 
-  #Initialize vector of significant lags of auto-correlation
+    #Initialize vector of significant lags of auto-correlation
 
-  sig.v <- rep(0, round(n/4))
+    sig.v <- rep(0, round(n/4))
 
-  #Identify the lag of the smallest significant auto-correlation coefficient
+    #Identify the number of contiguous significant serial correlations
 
-  for (i in 1:round(n/4)) {
-    if (-bd > ro[i] | bd < ro[i]) {
-      sig.v[i]<-ro[i]
+    for (i in 1:round(n/4)) {
+      if (-bd > ro[i] | bd < ro[i]) {
+        sig.v[i]<-ro[i]
+      }
     }
+
+    if (all(sig.v == 0)) {
+      min.sig<-0
+    } else {
+      min.sig.init<-rle(sig.v)
+      min.sig<-max(min.sig.init$lengths[min.sig.init$values != 0])
+    }
+
+    #Block length
+
+    bl.len <- min.sig + eta
+
   }
-
-  if (length(sig.v) == 1) {
-    min.sig<-0
-  } else {
-    min.sig <- max(which(sig.v %in% min(sig.v[sig.v > 0])))
-  }
-
-  #Block length
-
-  bl.len <- min.sig + eta
 
   #Block bootstrap using Mann Kendall
 
@@ -126,7 +135,7 @@ bbs <- function(x,ci=0.95,method=c("sr"),nsim=40,eta=1) {
     ub <- round(bbs.ci[2], digits = 7)
 
     cat(paste("Z-Value = ", z,
-              "Sen's slope = ", slp,
+              "Sen's Slope = ", slp,
               "S = ", S,
               "Kendall's Tau = ", Tau,
               "Kendall's Tau Empirical Bootstrapped CI =", sprintf("(%s,%s)",lb,ub),sep="\n"),sep="\n")
@@ -145,4 +154,5 @@ bbs <- function(x,ci=0.95,method=c("sr"),nsim=40,eta=1) {
               "Test Statistic Empirical Bootstrapped CI =", sprintf("(%s,%s)",lb,ub),sep="\n"),sep="\n")
   }
 }
+bbs(x)
 
